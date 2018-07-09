3843d83f3f9fb0e354abea7ca3075d5966b2d8b9
Mark Dewey <mark.t.dewey@gmail.com>
2018-07-09 12:42:41 -0400

    ready for deployment

--- src/pages/academy/success.js
+++ src/pages/academy/success.js
@@ -5,7 +5,7 @@ import Img from 'gatsby-image'
 
 const Card = ({ id, name, title, children, sizes }) => (
   <div className="card" id={`${id}`}>
-     <Img
+    <Img
       sizes={sizes}
       alt={`A portrait of ${name}`}
       title={`A portrait of ${name}`}
@@ -24,7 +24,7 @@ const Card = ({ id, name, title, children, sizes }) => (
   </div>
 )
 
-const SuccessStoriesPage = (props) => (
+const SuccessStoriesPage = props => (
   <div className="AcademyPage Success">
     <AcademyNavigation />
     <div className="wrap">
@@ -35,25 +35,25 @@ const SuccessStoriesPage = (props) => (
           name="Kristen Swan-Grashel"
           sizes={props.data.kristen.sizes}
         >
-        "When I first thought about going to a code school to change
-        careers, I knew it would be hard, especially with three young
-        children to care for. I also knew it would be worth it in the end.
-        The Iron Yard experience turned out to be just as hard as I ever
-        imagined, but also just as worth it. I accomplished what I set out
-        to do. And really, there are few things better in life than that.
-        Achieving a goal you've worked hard for."
+          "When I first thought about going to a code school to change I knew it
+          I knew it would be hard, especially with three young children to care
+          for. I also knew it would be worth it in the end. The Iron Yard
+          experience turned out to be just as hard as I ever imagined, but also
+          just as worth it. I accomplished what I set out to do. And really,
+          there are few things better in life than that. Achieving a goal you've
+          worked hard for."
         </Card>
 
-         <Card
+        <Card
           id="brandyn"
           name="Brandyn Sullins"
           sizes={props.data.brandyn.sizes}
         >
-        "This was one of the most intimidating, exhausting, and stressful
-        challenges I have ever taken on and I would not change my decision
-        to attend for anything. This gave me a home away from home, it
-        became a sanctuary where people of similar mind would share ideas
-        and learn from each other."
+          "This was one of the most intimidating, exhausting, and stressful
+          challenges I have ever taken on and I would not change my decision to
+          attend for anything. This gave me a home away from home, it became a
+          sanctuary where people of similar mind would share ideas and learn
+          from each other."
         </Card>
 
         <Card
@@ -61,23 +61,18 @@ const SuccessStoriesPage = (props) => (
           name="Valeria Benetti"
           sizes={props.data.valeria.sizes}
         >
-        "Of all the choices I have ever made, coming to this has to be one
-        of the greatest choices that my brain came up with. Not one day goes
-        by since graduating have I been extremely grateful to the school for
-        teaching me the valuable skill sets that I now know."
+          "Of all the choices I have ever made, coming to this has to be one of
+          the greatest choices that my brain came up with. Not one day goes by
+          since graduating have I been extremely grateful to the school for
+          teaching me the valuable skill sets that I now know."
         </Card>
 
-         {/* <Card
-          id="shaun"
-          name="Shaun Hailey"
-          sizes={props.data.shaun.sizes}
-        >
-        "I came to into this with zero programming background whatsoever.
-        After over a decade in finance, a layoff put me into a position
-        where I could explore a new career. This finally allowed me to do
-        what I had wanted to do for many years--learn how to code."
-        </Card> */}
-
+        <Card id="shaun" name="Shaun Hailey" sizes={props.data.shaun.sizes}>
+          "I came to into this with zero programming background whatsoever.
+          After over a decade in finance, a layoff put me into a position where
+          I could explore a new career. This finally allowed me to do what I had
+          wanted to do for many years--learn how to code."
+        </Card>
       </div>
 
       <p className="cta">
